ad591f966482cc1d37bcee82df8650afd6b3ca48
Juho Kesälä <elukka.elukka@gmail.com>
2018-11-06 18:52:51 +0200

    Add functionality to add ships to local database

--- application/ships/views.py
+++ application/ships/views.py
@@ -1,5 +1,6 @@
-from application import app
+from application import app, db
 from flask import render_template, request
+from application.ships.models import Ship
 
 @app.route("/")
 def index():
@@ -15,5 +16,14 @@ def ships_form():
 # This doesn't actually take the required information to make a ship yet, or do anything with it
 @app.route("/ships/", methods=["POST"])
 def ships_create():
-    print(request.form.get("name"))
+    ship = Ship(request.form.get("name"), request.form.get("cost"), request.form.get("propulsion_type"), request.form.get("move"), 
+                request.form.get("delta_v"), request.form.get("evasion_passive"), request.form.get("evasion_active"), 
+                request.form.get("evasion_endurance"), request.form.get("armor_front"), request.form.get("armor_sides"), 
+                request.form.get("armor_back"), request.form.get("weapon1_name"), request.form.get("weapon2_name"), 
+                request.form.get("weapon2_name")
+               )
+
+    db.session().add(ship)
+    db.session().commit()
+
     return "hello world!"
