8707b311d470683ada6a2ce2b69b7757316226d1
Juho Kesälä <elukka.elukka@gmail.com>
2018-11-13 19:46:58 +0200

    Add functionality for creating new users

--- application/auth/views.py
+++ application/auth/views.py
@@ -1,9 +1,9 @@
 from flask import render_template, request, redirect, url_for
 from flask_login import login_user, logout_user
 
-from application import app
+from application import app, db
 from application.auth.models import User
-from application.auth.forms import LoginForm
+from application.auth.forms import LoginForm, UserCreateForm
 
 @app.route("/auth/login", methods = ["GET", "POST"])
 def auth_login():
@@ -29,3 +29,20 @@ def auth_login():
 def auth_logout():
     logout_user()
     return redirect(url_for("index"))    
+
+@app.route("/auth/newuser", methods = ["GET", "POST"])
+def auth_newuser():
+    if request.method == "GET":
+        return render_template("/auth/newuser.html", form = UserCreateForm())
+
+    form = UserCreateForm()
+    # Validation goes here
+
+    new_user = User(form.name.data, form.username.data, form.password.data)
+    
+    db.session.add
+
+    db.session().add(new_user)
+    db.session().commit()
+
+    return redirect(url_for("index"))
