cf150cb4241759767b2cb851944bf92d12e5bdcd
Juho Kesälä <elukka.elukka@gmail.com>
2018-12-15 21:23:24 +0200

    Refactor weapon models to use inheritance

--- application/weapons/models.py
+++ application/weapons/models.py
@@ -1,80 +1,62 @@
 from application import db
+ 
+from sqlalchemy.ext.declarative import declared_attr
 
-# The weapon classes really should inherit from this abstract base class, but they
-# currently don't because I can't invoke the correct rituals to make SQLAlchemy
-# work right with this.
-"""
+# Abstract base model for weapon models
 class Weapon(db.Model):
     __abstract__ = True
-
+ 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(256), nullable=False)
+ 
+    @declared_attr
+    def ship_id(cls):
+        return db.Column(db.Integer, db.ForeignKey('ship.id'), nullable=False, index=True)
+ 
     def __init__(self, name, ship_id):
         self.name = name
         self.ship_id = ship_id
-"""
-
+ 
 # This class is a stub that is missing data that will eventually be provided in
 # another table'
-class Laser(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(256), nullable=False)
+class Laser(Weapon):
     laser_dmg_missile = db.Column(db.String(256), nullable=False)
-    ship_id = db.Column(db.Integer, db.ForeignKey('ship.id'), nullable=False, index=True)
-
+ 
     def __init__(self, name, dmg_missile, ship_id):
-        self.name = name
+        super().__init__(name, ship_id)
         self.laser_dmg_missile = dmg_missile
-        self.ship_id = ship_id
-
-class Missile(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(256), nullable=False)
+ 
+class Missile(Weapon):
     volley = db.Column(db.Integer, nullable=False)
     stores = db.Column(db.Integer, nullable=False)
-    ship_id = db.Column(db.Integer, db.ForeignKey('ship.id'), nullable=False, index=True)
-
+ 
     def __init__(self, name, volley, stores, ship_id):
-        self.name = name
-        self.ship_id = ship_id
+        super().__init__(name, ship_id)
         self.volley = volley
         self.stores = stores
-
-class CIWS(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(256), nullable=False)
+ 
+class CIWS(Weapon):
     dmg_missile = db.Column(db.String(256), nullable=False)
     dmg_ship = db.Column(db.String(256), nullable=False)
-    ship_id = db.Column(db.Integer, db.ForeignKey('ship.id'), nullable=False, index=True)
-
+ 
     def __init__(self, name, dmg_missile, dmg_ship, ship_id):
-        self.name = name
-        self.ship_id = ship_id
+        super().__init__(name, ship_id)
         self.dmg_missile = dmg_missile
         self.dmg_ship = dmg_ship
-
-class AreaMissile(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(256), nullable=False)
+ 
+class AreaMissile(Weapon):
     am_range = db.Column(db.Integer, nullable=False)
     dmg_missile = db.Column(db.String(256), nullable=False)
     dmg_ship = db.Column(db.String(256), nullable=False)
-    ship_id = db.Column(db.Integer, db.ForeignKey('ship.id'), nullable=False, index=True)
-
+ 
     def __init__(self, name, am_range, dmg_missile, dmg_ship, ship_id):
-        self.name = name
-        self.ship_id = ship_id
+        super().__init__(name, ship_id)
         self.am_range = am_range
         self.dmg_missile = dmg_missile
         self.dmg_ship = dmg_ship
-
+ 
 # This class is a stub that is missing data that will eventually be provided in
 # another table
-class Ewar(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(256), nullable=False)
-    ship_id = db.Column(db.Integer, db.ForeignKey('ship.id'), nullable=False, index=True)
-
+class Ewar(Weapon):
     def __init__(self, name, ship_id):
-        self.name = name
-        self.ship_id = ship_id
+        super().__init__(name, ship_id)
