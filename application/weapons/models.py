f6137955d37c2403b6c84e2ae67ae55592ac6974
Juho Kesälä <elukka.elukka@gmail.com>
2018-12-22 23:04:43 +0200

    Add laser range point and ewar ability models

--- application/weapons/models.py
+++ application/weapons/models.py
@@ -17,16 +17,27 @@ class Weapon(db.Model):
         self.name = name
         self.ship_id = ship_id
  
-# This class is a stub that is missing data that will eventually be provided in
-# another table'
 class Laser(Weapon):
     turreted = db.Column(db.Boolean, nullable=False)
     laser_dmg_missile = db.Column(db.String(256), nullable=False)
+
+    rangepoints = db.relationship('LaserRangePoint', backref='laser', lazy=True)
  
     def __init__(self, name, turreted, dmg_missile, ship_id):
         super().__init__(name, ship_id)
         self.turreted = turreted
         self.laser_dmg_missile = dmg_missile
+
+class LaserRangePoint(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    lrange = db.Column(db.Integer, nullable=False)
+    dmg = db.Column(db.Integer, nullable=False)
+    laser_id = db.Column(db.Integer, db.ForeignKey('laser.id'), nullable=False, index=True)
+
+    def __init__(self, lrange, dmg, laser_id):
+        self.lrange = lrange
+        self.dmg = dmg
+        self.laser_id = laser_id
  
 class Missile(Weapon):
     volley = db.Column(db.Integer, nullable=False)
@@ -57,8 +68,19 @@ class AreaMissile(Weapon):
         self.dmg_missile = dmg_missile
         self.dmg_ship = dmg_ship
  
-# This class is a stub that is missing data that will eventually be provided in
-# another table
 class Ewar(Weapon):
+    abilities = db.relationship('EwarAbility', backref='ewar', lazy=True)
+
     def __init__(self, name, ship_id):
         super().__init__(name, ship_id)
+
+class EwarAbility(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    erange = db.Column(db.Integer, nullable=False)
+    ability = db.Column(db.String(256), nullable=False)
+    ewar_id = db.Column(db.Integer, db.ForeignKey('ewar.id'), nullable=False, index=True)
+
+    def __init__(self, erange, ability, ewar_id):
+        self.erange = erange
+        self.ability = ability
+        self.ewar_id = ewar_id
